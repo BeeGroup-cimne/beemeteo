f56ba3a801f8034c33234646548a3b15d486b942
marcgironaupc <marc.girona.badia@upc.edu>
2023-10-09 16:57:15 +0200

    Upgrade forecast

--- tests/test_appleweather.py
+++ tests/test_appleweather.py
@@ -5,13 +5,15 @@ import json
 from beemeteo.sources.appleweather import AppleWeather
 
 
-def test_apple_weather():
+def test_apple_weather_historic():
     source = AppleWeather(json.load(open("config.json")))
+    latitude = 41.540
+    longitude = 2.454
     data = source.get_historical_data(
-        latitude=-30.3183,
-        longitude=131.8894413,
-        date_from=datetime.datetime(2022, 1, 1),
-        date_to=datetime.datetime(2022, 1, 9),
+        latitude=latitude,
+        longitude=longitude,
+        date_from=datetime.datetime(2023, 9, 16),
+        date_to=datetime.datetime(2023, 9, 18),
     )
     expected = pd.read_csv("tests/b2back/darksky.csv")
     columns_to_compare = [
@@ -22,7 +24,17 @@ def test_apple_weather():
         "windGust",
         "windSpeed",
     ]
+    print(data)
+    assert data.equals(expected)
 
-    source.collect_forecasting(41.6100308, 0.6307409)
-    x = source.get_forecasting_data(41.6100308, 0.6307409, datetime.datetime(2022, 1, 1), datetime.datetime(2022, 3, 1))
-    #assert_frame_equal(data[columns_to_compare], expected[columns_to_compare])
+def test_apple_weather_forecast():
+    source = AppleWeather(json.load(open("config.json")))
+    latitude = 41.540
+    longitude = 2.454
+    source.collect_forecasting(latitude, longitude)
+    forecast = source.get_forecasting_data(latitude, longitude,
+                                           date_from = datetime.datetime.now()-datetime.timedelta(days=5), 
+                                           date_to = datetime.datetime.now())
+
+    print(forecast)
+    assert forecast.equals(expected)
